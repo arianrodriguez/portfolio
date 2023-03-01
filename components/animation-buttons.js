a19ce80dd46c74564a1671fd180d1702a9bd233b
Arian <arianmrv12@gmail.com>
2023-03-01 18:04:43 -0500

    animation buttons fixed so that they work in all the html

--- components/animation-buttons.js
+++ components/animation-buttons.js
@@ -1,14 +1,18 @@
-const first_button = document.querySelector('.box-buttons').firstElementChild;
-const second_button = document.querySelector('.box-buttons').lastElementChild;
-const buttons = [first_button, second_button];
-
-buttons.forEach((button) => {
-    button.addEventListener('mouseover', () => {
-        first_button.classList.add('button-light');
-        second_button.classList.remove('button-light');
+const box_buttons = document.querySelectorAll('.box-buttons');
+box_buttons.forEach((box) => {
+    const first_button = box.firstElementChild;
+    const second_button = box.lastElementChild;
+    const buttons = [first_button, second_button];
+    
+    buttons.forEach((button) => {
+        button.addEventListener('mouseover', () => {
+            first_button.classList.add('button-light');
+            second_button.classList.remove('button-light');
+        });
+        button.addEventListener('mouseout', () => {
+            first_button.classList.remove('button-light');
+            second_button.classList.add('button-light');
+        });
     });
-    button.addEventListener('mouseout', () => {
-        first_button.classList.remove('button-light');
-        second_button.classList.add('button-light');
-    })
 })
+
