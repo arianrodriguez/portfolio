f5ff6a448e0824968e7dd0272e28c313fb17d7f9
Arian <arianmrv12@gmail.com>
2023-03-02 15:15:06 -0500

    buttons with links added

---
+++ components/buttons-links.js
@@ -0,0 +1,18 @@
+const buttons = document.querySelectorAll('.button');
+
+const links = {
+    cv: 'https://drive.google.com/file/d/1HznJPnFa53yveJleuDKj4TddCu6KHykt/view',
+    psw_demo: 'https://arianrodriguez.github.io/password-generator/',
+    psw_gh: 'https://github.com/arianrodriguez/password-generator',
+    encryp_demo: 'https://arianrodriguez.github.io/encrypting-text/',
+    encryp_gh: 'https://github.com/arianrodriguez/encrypting-text',
+    ss_demo: 'https://www.youtube.com/watch?v=gZjYU_DTVuQ',
+    ss_gh: 'https://github.com/arianrodriguez/staysafe'
+}
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if(links[button.dataset.btn])
+            window.open(links[button.dataset.btn], '_blank');
+    }) 
+});
